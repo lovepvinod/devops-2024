0581e8ce1eb88923df4def92545d2913a2a67bfb
lovepvinod <39132993+lovepvinod@users.noreply.github.com>
2024-03-16 01:52:51 +0000

    Setting right.

--- main.py
+++ main.py
@@ -11,13 +11,13 @@ import click
 @click.argument("b", type=int)
 def add_cli(a, b):
     """Add two numbers
-    
+
     Args:
         a (int): first number
         b (int): secod number
     """
     click.echo(add(a, b))
-    var=
+
 
 if __name__ == "__main__":
     # pylint: disable=no-value-for-parameter
